78ba865e7f3f4173dff02e590fc3fe2b03a1aeb0
henmikazuki <minskeh@gmail.com>
2021-02-08 21:49:37 +0900

    微修正

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -17,11 +17,11 @@
                 <div class="md-form">
                   <label for="name">ユーザー名</label>
                   <input class="form-control" type="text" id="name" name="name" required value="{{ old('name') }}">
-                  <small>英数字3～16文字(登録後の変更はできません)<small>
+                  <small>英数字3〜16文字(登録後の変更はできません)</small>
                 </div>
                 <div class="md-form">
                   <label for="email">メールアドレス</label>
-                  <input class="form-control" type="text" id="email" name="email" required value="{{ old('email') }}">
+                  <input class="form-control" type="text" id="email" name="email" required value="{{ old('email') }}" >
                 </div>
                 <div class="md-form">
                   <label for="password">パスワード</label>
